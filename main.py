b2bac0c775de10441680ef17d4aa96e9104e10eb
Sergei Kuksov <ser.kuksov@mail.ru>
2022-12-20 21:08:44 +0300

    Функции для запуска парсера

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import json
 import logging
+import os
+
 from exel_operations import get_not_filed_petitions, save_filed_petition_in_excel
-from parser_gosuslug import file_petition
+from parser.parser_gosuslogi import Parser_gosuslugi
 
 
 def log():
@@ -16,29 +19,75 @@ def log():
     logging.getLogger("pymorphy2").setLevel(logging.ERROR)
 
 
-def file_petitions(not_filed_petitions: list[dict]):
-    """Генератор поданных ходатайств который перебирает список словарей с параметрами ходатайства,
+def get_config():
+    try:
+        with open('config.txt', 'r', encoding='utf-8') as file:
+            config = json.load(file)
+    except FileNotFoundError:
+        logging.error('Ошибка!!! Не удалось открыть файл конфигурации')
+        raise FileNotFoundError
+    list_config = list(config.keys())
+    verification_list_config = [
+        "LOGIN",
+        "PASSWORD",
+        "TIMEOUT",
+        "ORGANIZATION",
+        "NAME_FILE_ORDER"
+    ]
+    if list_config == verification_list_config:
+        logging.info(f'Получены параметры конфигурации')
+        return config
+    else:
+        logging.error('Ошибка!!! В файле конфигурации нет части настроек')
+        raise ValueError()
+
+
+def get_path_file(name_file: str) -> str:
+    """Проверяет наличие файла в текущей директории запуска скрипта.
+    Возвращает путь к файлу."""
+    path_script = os.path.dirname(os.path.abspath(__file__))
+    path_file = os.path.join(path_script, name_file)
+    if not os.path.exists(path_file):
+        logging.error(f'Не найден файл приказа с названием {name_file}')
+        raise FileNotFoundError
+    logging.info(f'Получен путь к файлу приказа')
+    return path_file
+
+
+def filing_petitions():
+    """Функция для подачи ходатайств.
+    Перебирает список словарей с параметрами ходатайства, прочитанный из эксель,
     вызывает функцию подачи ходатайства, получает ответ в виде параметров с
-    датой отправки и номером заявления, и возвращает поданное ходатайство"""
+    датой отправки и номером заявления. Сохраняет данные в эксель"""
+    config = get_config()
+    not_filed_petitions = get_not_filed_petitions()
+    if not not_filed_petitions:
+        exit()
+    parser = Parser_gosuslugi(login=config['LOGIN'],
+                              password=config['PASSWORD'],
+                              organization=int(config['ORGANIZATION']),
+                              timeout=int(config['TIMEOUT']))
+    name_file = config['NAME_FILE_ORDER']
+    path_file = get_path_file(name_file=name_file)
     i = 1
     for petition in not_filed_petitions:
-        params_petition = file_petition(number_ip=petition['Номер ИП'],
-                                        text_petition=petition['Ходатайство'])
+        logging.info(f'Приступаю к подаче обращения {i}')
+        params_petition = parser.register_petition(number_ip=petition['Номер ИП'],
+                                                   text_petition=petition['Ходатайство'],
+                                                   path_file=path_file)
         filed_petition = petition | params_petition
-        logging.info(f'Подано {i} ходатайств из {len(not_filed_petitions)}')
+        logging.info(f'Подано {i} обращение из {len(not_filed_petitions)}')
         i += 1
-        yield filed_petition
+        save_filed_petition_in_excel(filed_petition)
 
 
 def main():
     log()
-    not_filed_petitions = get_not_filed_petitions()
-    for filed_petition in file_petitions(not_filed_petitions=not_filed_petitions):
-        save_filed_petition_in_excel(filed_petition)
+    filing_petitions()
 
 
 if __name__ == '__main__':
     try:
         main()
-    except:
-        logging.exception()
+    except Exception as ex:
+        logging.exception(ex)
